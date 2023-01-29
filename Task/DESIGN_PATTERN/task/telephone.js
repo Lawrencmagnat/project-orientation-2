380fde676f7bb96625e1db0507aae8eaa0c85604
[Lawrencmagnat] <[chukwulawrencep@gmail.com]>
2023-01-29 19:45:05 +0100

    Learning/task in design pattern correct

--- Task/DESIGN_PATTERN/task/telephone.js
+++ Task/DESIGN_PATTERN/task/telephone.js
@@ -1,87 +1,52 @@
-class telephoneUpdate {
-    constructor() {
-        this.observers = new set();
-        this.Email = new set();
-        this.Wallet = new set();
+class TelephoneUpdate{
+    constructor(){
+        this.phoneNumbers = [];
+        this.observers = [];
 
     }
 
-
-    add(number) {
-        this.observers.add(number)
+    addPhoneNumber(number){
+        this.phoneNumbers.push(number);
     }
 
-    remove(number) {
-        this.observers.delete(number)
+    removePhoneNumber(number){
+        this.phoneNumbers = this.phoneNumbers.filter(b => b !==number);
     }
 
-    dial(phonenumber) {
-        this.observer.dial(phonenumber)
+    dialnumber(number){
+        // if(!this.phoneNumbers.includes(number)){
+        //     throw new Error('Number not correctly formatted')
+
+        // }
+        this.phoneNumbers.push(number);
+
+        this.observers.forEach(observer => observer.onDial(number));
+    }
+    addObserver(observer){
+        this.observers.push(observer);
     }
 
-  
+    removeObserver(observer){
+        this.addObservers = this.observers.filter( c => c !== observer);
+    }
 }
 
-class Phonenumber extends telephoneUpdate{
-    onDial(number) {
-        console.log(`The number is ${number} previously`)
+
+class observer extends TelephoneUpdate{
+    onDial(number){
+        console.log(`The number ${number} has been dialed`);
     }
-
 }
-const phone = new telephoneUpdate ();
-phone.number('44883248');
 
-const observer = new Phonenumber();
-phone.phonenumber
+const phone = new TelephoneUpdate();
 
-console.log(telephoneUpdate)
-// function subject(){
-//     this.observers = []
-// }
+phone.addPhoneNumber('+23444883248');
 
 
-// subject.protype = {
-//     addPhoneNumber: function (fn) {
-//         this.observers.push(fn)
-//     },
+const observed = new observer();
+phone.addPhoneNumber(observed);
 
-//     removePhoneNumber: function (fnToRemove) {
-//         this.observers = this.observers.filter(fn => {
-//             if (fn != fnToRemove)
-//                 return fn
+phone.dialnumber('234568765456');
 
-//         })
-
-
-//     },
-
-//     dialPhoneNumber: function (fn) {
-//         this.observers.call(fn)
-//     },
-
-//     fire: function () {
-//         this.observer.forEach(fn => {
-//             fn.call()
-//         })
-//     }
-
-
-
-// }
-
-// const observer = new subject()
-
-// function observer1(){
-//     console.log('call: 08144883248')
-// };
-
-
-// function observer2(){
-//     console.log('Now Dialling: +234_70232323 ')
-// }
-
-// subject.dialPhoneNumber(observer1)
-
-// subject.fire()
-
+console.log(phone, observed)
 
