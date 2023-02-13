6ee0d9d7905012b765bb5ef09db20e6d3eb548a6
[Lawrencmagnat] <[chukwulawrencep@gmail.com]>
2023-02-13 12:54:34 +0100

    Wallet

---
+++ Desktop/understand/my-app/src/App.js
@@ -0,0 +1,29 @@
+// import React, { useEffect, useState} from "react";
+// import "./App.css";
+// import {ethers} from "ethers";
+// import abi from "./abi.json";
+// // import waveporta
+
+// const App = () => {
+//     const [currentAccount, setCurrentAccount] = useState("");
+//     const 
+// }
+
+
+
+// import React from 'react';
+const create = () => {
+    return (
+        <div className="create">
+            <h2>Wallet Athentication</h2>
+            <p>Connect Wallet</p>
+        <input type="text" placeholder="connect wallet"></input>
+        <br></br>
+        <br></br>
+        <textarea placeholder="send us a message here"></textarea>
+        <button>Send</button>
+
+        </div>
+
+    );
+}
