51bb715af016c88e39a58c9d113c31122260eb03
[Lawrencmagnat] <[chukwulawrencep@gmail.com]>
2023-02-13 16:06:05 +0100

    wallet

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import connect from './connect';
+
+function App(){
+    return(
+        <connect />
+        
+
+    )
+}
+
+export default App;
---
+++ src/connect.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function connect() {
+  return (
+   <div>
+    Hello world
+   </div>
+  )
+}
